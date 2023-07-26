961c4da40609b44e687d2f0ecbfc62f94d28d785
PlashSpeed-Aiman <maiman.ar@live.iium.edu.my>
2023-07-26 16:02:58 +0800

    release build

--- main.go
+++ main.go
@@ -6,13 +6,30 @@ import (
 	"gopkg.in/yaml.v3"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 type Scaffold struct {
+	ProjectLocation  string   `yaml:"projectLocation"`
+	ConnectionString string   `yaml:"connectionString"`
+	Provider         string   `yaml:"provider"`
+	Tables           []string `yaml:"tables"`
+	Args             []string `yaml:"args"`
+}
+
+// Will be used soon
+type ScaffoldV2 struct {
 	ProjectLocation  string `yaml:"projectLocation"`
-	ConnectionString string `yaml:"connectionString"`
-	Provider         string `yaml:"provider"`
-	Tables           []string
+	ConnectionString struct {
+		DataSource string `yaml:"dataSource"`
+		Username   string `yaml:"username"`
+		Password   string `yaml:"password"`
+		Database   string `yaml:"database"`
+	} `yaml:"connectionString"`
+	ConnectionStringOptions []string `yaml:"connectionStringOptions"`
+	Provider                string   `yaml:"provider"`
+	Tables                  []string `yaml:"tables"`
+	Args                    []string `yaml:"args"`
 }
 
 func main() {
@@ -28,10 +45,63 @@ func main() {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	cmd := exec.Command("powershell")
+	res := JoinString(&scaffold)
+	fmt.Println(res)
+	cmd := exec.Command("powershell", "cd "+scaffold.ProjectLocation+";", "dotnet ef dbcontext scaffold ", res)
 	out, err := cmd.Output()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	fmt.Println(string(out))
+	fmt.Println("Press Any Key To Continue")
+	fmt.Scanf(`%s`)
+}
+
+func JoinString(scaffold *Scaffold) string {
+	var sb strings.Builder
+	sb.WriteString(scaffold.ConnectionString)
+	sb.WriteString(" ")
+	sb.WriteString(scaffold.Provider)
+	sb.WriteString(" ")
+	for _, str := range scaffold.Tables {
+		sb.WriteString("-t ")
+		sb.WriteString(str)
+	}
+	for _, str := range scaffold.Args {
+		sb.WriteString(" ")
+		sb.WriteString(str)
+	}
+	return sb.String()
+
+}
+func JoinStringV2(scaffold *ScaffoldV2) string {
+	var sb strings.Builder
+	sb.WriteString(JoinConnectionString(scaffold))
+	sb.WriteString(" ")
+	sb.WriteString(scaffold.Provider)
+	sb.WriteString(" ")
+	for _, str := range scaffold.Tables {
+		sb.WriteString("-t ")
+		sb.WriteString(str)
+	}
+	for _, str := range scaffold.Args {
+		sb.WriteString(" ")
+		sb.WriteString(str)
+	}
+
+	return sb.String()
+
+}
+func JoinConnectionString(v2 *ScaffoldV2) string {
+	var sb strings.Builder
+	sb.WriteString("\"")
+	sb.WriteString("data source=" + v2.ConnectionString.DataSource + ";")
+	sb.WriteString("user id=" + v2.ConnectionString.Username + ";")
+	sb.WriteString("password=" + v2.ConnectionString.Password + ";")
+	sb.WriteString("database=" + v2.ConnectionString.Database + ";")
+	for _, str := range v2.ConnectionStringOptions {
+		sb.WriteString(str + ";")
+	}
+	sb.WriteString("\"")
+	return sb.String()
 }
